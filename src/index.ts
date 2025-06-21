3bc9a0b016cc2e562cf86dc425b04ce666434a35
theneuetimes <wygonski@gmail.com>
2025-06-21 22:01:14 +0200

    fix import issues

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+// import { DrawFn } from "./types/q5-canvas";
+// export type { SharedState, Q5CanvasProps } from "./types/q5-canvas";
+// export { default as Q5Canvas } from "./q5-canvas";
+// export { useCanvasState } from "./hooks/use-canvas-state";
+// export const createDraw = (fn: DrawFn) => fn;
 import { DrawFn } from "./types/q5-canvas";
-export type { SharedState, Q5CanvasProps } from "./types/q5-canvas";
-export { default } from "./q5-canvas";
-export { useCanvasState } from "./hooks/use-canvas-state";
+import Q5Canvas from "./q5-canvas";
+export default Q5Canvas;
+
+export * from "./hooks/use-canvas-state";
+export * from "./types/q5-canvas";
 export const createDraw = (fn: DrawFn) => fn;
