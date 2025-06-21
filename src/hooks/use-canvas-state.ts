29605378fde942980a04a57668ec338c3364f211
theneuetimes <wygonski@gmail.com>
2025-06-21 22:36:34 +0200

    add proper types to use-canvas-state hook

--- src/hooks/use-canvas-state.ts
+++ src/hooks/use-canvas-state.ts
@@ -1,14 +1,20 @@
 import { useMemo } from "react";
 import { createCanvasState } from "../lib/canvas-state";
 
-const globalStates = new Map<string, any>();
+type CanvasState<T> = {
+  get: () => T;
+  set: (partial: Partial<T>) => void;
+  subscribe: (cb: (state: T) => void) => () => boolean;
+};
 
-export const useCanvasState = <T>(id: string, initial: T) => {
+const globalStates = new Map<string, CanvasState<any>>();
+
+export const useCanvasState = <T>(id: string, initial: T): CanvasState<T> => {
   if (import.meta.hot) {
     if (!globalStates.has(id)) {
       globalStates.set(id, createCanvasState<T>(initial));
     }
-    return globalStates.get(id);
+    return globalStates.get(id) as CanvasState<T>;
   }
 
   return useMemo(() => createCanvasState<T>(initial), []);
