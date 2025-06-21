98eba264fedb6da301467e92878d0249ac55bfa8
theneuetimes <wygonski@gmail.com>
2025-06-21 20:42:25 +0200

    implement hmr state loss protection

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,25 @@ import { SharedState } from "./types/q5-canvas";
 import { useState } from "react";
 import { useCanvasState } from "./hooks/use-canvas-state";
 
+const draw = (p: q5, state: SharedState) => {
+  p.background(0);
+  p.fill(255);
+
+  for (let i = 0; i < state.count; i++) {
+    p.ellipse(i * 10 + 5, 50, 10, 10);
+  }
+
+  const text = `Count is: ${state.count}`;
+
+  p.textSize(24);
+  p.text(text, p.width / 2 - p.textWidth(text) / 2, p.height / 2);
+
+  p.ellipse(p.mouseX, p.mouseY, 20, 20);
+};
+
 function App() {
   const [test, setTest] = useState(0);
   const canvasState = useCanvasState({ count: 0 });
-
-  const draw = (p: q5, state: SharedState) => {
-    p.background(0);
-    p.fill(255);
-
-    for (let i = 0; i < state.count; i++) {
-      p.ellipse(i * 10 + 5, 50, 10, 10);
-    }
-
-    const text = `Count is: ${state.count}`;
-
-    p.textSize(24);
-    p.text(text, p.width / 2 - p.textWidth(text) / 2, p.height / 2);
-
-    p.ellipse(p.mouseX, p.mouseY, 20, 20);
-  };
 
   return (
     <div
--- src/hooks/use-canvas-state.ts
+++ src/hooks/use-canvas-state.ts
@@ -1,8 +1,16 @@
 import { useMemo } from "react";
 import { createCanvasState } from "../lib/canvas-state";
 
+let _state: ReturnType<typeof createCanvasState> | null = null;
+
 export const useCanvasState = <T extends object>(
   initial: T extends object ? T : never
 ) => {
+  if (import.meta.hot) {
+    if (!_state) _state = createCanvasState(initial);
+    return _state;
+  }
+
   return useMemo(() => createCanvasState(initial), []);
+  //   return useMemo(() => createCanvasState(initial), []);
 };
